05308cfdefe195cacbf23073983b98c575d4f63f
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-18 15:10:12 -0700

    work

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - print all alphabet in lower case
+ *print_alphabet - print all alphabet in lower case
 */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 char anum;
 for (anum = 'a'; anum <= 'z'; anum++)

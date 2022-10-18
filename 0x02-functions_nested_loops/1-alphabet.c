01bce359cf2656245040b7b4de2e1bac9eb5c853
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-18 14:48:46 -0700

    bettydoings

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,8 @@
 */
 void print_alphabet_x10(void)
 {
-char anum, c;
-c = 0;
-while (c < 10)
-{
+char anum;
 for (anum = 'a'; anum <= 'z'; anum++)
-{
 _putchar (anum);
-}
-c++;
 _putchar('\n');
-}
 }

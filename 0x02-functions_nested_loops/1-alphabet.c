cf226687da22bb02173fac9968e154e25c480639
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-18 14:27:35 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print all alphabet in lower case
+ *Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char anum, c;
+c = 0;
+while (c < 10)
+}
+for (anum = 'a'; anum <= 'z'; anum++)
+}
+_putchar (anum);
+}
+c++;
+_putchar('\n');
+}
+}

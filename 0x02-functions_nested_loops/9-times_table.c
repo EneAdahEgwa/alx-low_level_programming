5674df8fde4cb397a48dfa1ed2944d97c296b20d
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-19 01:26:09 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ for (row = 0; row <= 9; row++)
 _putchar('0');
 _putchar(',');
 _putchar(' ');
-for (cone = 1; cone <= 9; cone++)
+for (col = 1; col <= 9; col++)
 {
 d = (row * col);
 if ((d / 10) > 0)

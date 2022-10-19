460a83ae591d1dab501a7b487f553140f005c8d8
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-19 03:34:14 -0700

    fibbo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,14 +1,15 @@
-#include "main.h"                                                                                          
-#include <stdio.h>                                                                                         
-/**                                                                                                        
- *main - finds and prints the first 98 Fibonacci numbers*starting with 1 and 2*followed by a new line       Return: ALways 0 (Success)
- */
+#include "main.h"                                                        
+#include <stdio.h>
+/** 
+*main - finds and prints the first 98 Fibonacci numbers*starting with 1 and 2*followed by a new line
+*Return: ALways 0 (Success)
+*/
 int main(void)
 {
 unsigned long int a, b, c, b1, b2, c1, c2;
-a = 1;                                                                         
-b = 2;
-printf("%lu", b);                                                                                          
+b = 1;
+c = 2;
+printf("%lu", b); 
 for (a = 1; a < 91; a++)
 {
 printf(", %lu", c);
@@ -21,8 +22,8 @@ b1 = b / 1000000000;
 b2 = b % 1000000000;
 for (a = 92; a < 99; ++a)
 {
-printf(", %lu", b1 + (b2 / 1000000000));
-printf("%lu", b2 % 1000000000);
+printf(", %lu", c1 + (c2 / 1000000000));
+printf("%lu", c2 % 1000000000);
 b1 = b1 + c1;
 c1 = b1 - c1;
 b2 = b2 + c2; 

7273d356da720bd6ffa9c392cd6ffee74310e611
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-19 01:24:33 -0700

    9times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/*
+ *times_table - a function that prints the 9 times_table, starting with 0
+ *row = row, col = column, d = digits of current result
+ *Return: times table
+ *add extra space past single digit
+*/
+void times_table(void)
+{
+int row, col, d;
+for (row = 0; row <= 9; row++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+for (cone = 1; cone <= 9; cone++)
+{
+d = (row * col);
+if ((d / 10) > 0)
+{
+_putchar((d / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((d % 10) + '0');
+if (col < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}

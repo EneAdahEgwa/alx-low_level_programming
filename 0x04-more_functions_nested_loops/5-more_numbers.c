7e6921ecff6cb15835c4d2a4f25b38cc6cd9c243
EneAdahEgwa <eneadahegwa@gmail.com>
2022-10-20 10:19:25 -0700

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,10 @@ for (i = 0; i <= 14; i++)
 {
 if (i >= 10)
 {
-_putchar((i /10) + '0');
+_putchar((i / 10) + '0');
 }
-_putchar((i % 10) +'0');
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
 }
 }

e9a66338474ae133e1b97b6f74f39a959354502b
EneAdahEgwa <eneadahegwa@gmail.com>
2022-11-07 15:04:34 -0800

    okay

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,18 +13,11 @@ int i, j, sum;
 sum = 0;
 for (i = 1; i < argc ; i++)
 {
-for (j = 0; argv[i][j] != '\0' ; j++)
-{
-if (argv[i][j] < 47 || argv[i][j] > 57)
-{
-{
-printf("Error\n");
-return (1);
-}
-sum = sum + atoi(argv[i]);
+for (j = 0; argv[i][j]; j++)
+if (argv[i][j] < '0' || argv[i][j] > '9')
+return(printf("Error\n"), 1);
+sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
 return (0);
-}
-}
 }

c746640e2f1deba8611b75a1a1e001850e20b19d
EneAdahEgwa <eneadahegwa@gmail.com>
2022-11-07 15:10:01 -0800

    betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ for (i = 1; i < argc ; i++)
 {
 for (j = 0; argv[i][j]; j++)
 if (argv[i][j] < '0' || argv[i][j] > '9')
-return(printf("Error\n"), 1);
+return (printf("Error\n"), 1);
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);

b4f0eaa12ebbe502a5d9863b84f7ca8acf1a2f00
EneAdahEgwa <eneadahegwa@gmail.com>
2022-11-09 00:13:00 -0800

    hello

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,28 +9,17 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char *array;
+char *ptr;
 unsigned int i;
-i = 0;
-if (size == 0)
+ptr = malloc(size);
+if (size == 0 || ptr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
 {
-return ('\0');
+ptr[i] = c;
 }
+if (!ptr)
+return (NULL);
 else
-{
-array = malloc(sizeof(char) * size);
-if (*array == '\0')
-{
-return ('\0');
-}
-else
-{
-while (i < size)
-{
-array[i] = c;
-i++;
-}
-}
-return (array);
-}
+return (ptr);
 }
